29fe7557b4921ff8653cdc6c59e7bcb3a4ab7367
Shubhang <abhayshubhang@gmail.com>
2025-11-14 09:31:16 +0530

    Corrected server.js and imported userRouter and installed missing modules

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,16 @@ const express = require('express')
 const cors = require('cors')
 
 //user defined modules
-const myAuth = require('./utils/auth')
+const authorization = require('./routes/authorization')
 const userRouter = require('./routes/user')
-const courseRouter = require('./routes/course')
 
 const app = express()
 
 //middleware
 app.use(cors())
-app.use('/image', express.static('images'))
 app.use(express.json())
-app.use(myAuth)
+app.use(authorization)
 app.use('/user', userRouter)
-app.use('/course', courseRouter)
 
 app.listen(4000, 'localhost', () => {
     console.log('server started at port 4000')
